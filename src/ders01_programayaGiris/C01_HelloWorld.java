c94fffc1aa6ceff7aec3ae95da02b3f0e0fbbd5b
YusuffKOK <yusufkok2164@gmail.com>
2022-10-31 13:32:37 +0300

    JavaBulutluozDersleri

--- src/ders01_programayaGiris/C01_HelloWorld.java
+++ src/ders01_programayaGiris/C01_HelloWorld.java
@@ -2,7 +2,7 @@ package ders01_programayaGiris;
 
 public class C01_HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World"); // Bunu yazmayan yazılımcı olamazmıs hahaha
+        System.out.println("Hello World"); // Bunu yazmayan yazılımcı olamazmıs
         // En iyi kod, calısan kodtur. Daha da iyisi hem calısan hemde anlasilabilir kod.
         // proje calısmalrında ekip halinde calisacagimiz icin bizim yazdigimiz kodu tüm ekip anlayabilmelidir
 
