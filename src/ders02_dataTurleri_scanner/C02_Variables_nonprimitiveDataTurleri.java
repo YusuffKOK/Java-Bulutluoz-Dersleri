14b026586a0dee0e51e46f67f22fdb49247a3359
YusuffKOK <yusufkok2164@gmail.com>
2022-11-09 22:14:38 +0300

    Java Dersleri Bulutluoz

---
+++ src/ders02_dataTurleri_scanner/C02_Variables_nonprimitiveDataTurleri.java
@@ -0,0 +1,20 @@
+package ders02_dataTurleri_scanner;
+
+public class C02_Variables_nonprimitiveDataTurleri {
+    public static void main(String[] args) {
+        // Nonprimitive Data turleri
+        // primitive data turleri java tarafından oluşturulmuşlardır.Biz primitive data turu oluşturamayız.
+        //Nonprimitive data turleri java tarafından oluşturulanlar oldugu gibi bizde oluşturabiliriz.
+        // data Türü isimleri büyük harfle başlar String ,Arrays ,Object gibi
+        // datanın hafızada kapladığı yer içeriğine göre degisir.
+
+
+        String isim = "Yusuf KöK";
+        String adres = "Kocasinan / Kayseri";
+        System.out.println(isim.toUpperCase());
+        System.out.println(isim.toLowerCase());
+
+
+
+    }
+}
---
+++ src/ders02_dataTurleri_scanner/C03_Naming_Convention.java
@@ -0,0 +1,9 @@
+package ders02_dataTurleri_scanner;
+
+public class C03_Naming_Convention {
+    public static void main(String[] args) {
+        // 1-) Variables isimleri case sensitivedir. Not ,NOT,nOT birbirinden farklıdır.
+
+
+    }
+}
